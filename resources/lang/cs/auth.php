c65d54e4da92017ed19b291699414a141791f0a7
Tonny Kosgey <tonnykosgey7@gmail.com>
2025-08-13 01:13:08 +0300

    first commit

---
+++ resources/lang/cs/auth.php
@@ -0,0 +1,19 @@
+<?php 
+return [
+  'failed' => 'Tyto přihlašovací údaje neodpovídají našim záznamům.',
+  'companyAccountDisabled' => 'Váš firemní účet je deaktivován.',
+  'failedLoginDisabled' => 'Přihlášení bylo zakázáno vaším administrátorem',
+  'failedBlocked' => 'Váš účet je deaktivován!. Obraťte se na svého správce, aby ji povolil.',
+  'failedCompanyUnapproved' => 'Vaše společnost ještě není schválena!. Počkejte prosím na schválení společnosti.',
+  'throttle' => 'Příliš mnoho pokusů o přihlášení. Zkuste to znovu za :seconds sekund.',
+  'recaptchaFailed' => 'Recaptcha není ověřena.',
+  'sociaLoginFail' => 'Váš účet neexistuje. Prosím zaregistrujte se',
+  'loginWrongCompany' => 'Snažíte se přihlásit do nesprávné společnosti.',
+  'signInGoogle' => 'Přihlaste se pomocí Google',
+  'signInFacebook' => 'Přihlásit se pomocí Facebooku',
+  'signInLinkedin' => 'Přihlaste se pomocí LinkedIn',
+  'signInTwitter' => 'Přihlásit se přes Twitter',
+  'useEmail' => 'nebo použijte e-mailovou adresu',
+  'email' => 'Emailová adresa',
+  'next' => 'další',
+];
